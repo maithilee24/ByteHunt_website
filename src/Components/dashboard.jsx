cfc41839ce97574134150082a12ba8ac327fa78a
Dev <devsa2067@gmail.com>
2024-01-29 12:40:40 +0530

    fifth commit

--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -8,43 +9,49 @@ const Dashboard = () => {
       </h1>
       <img
         src="/bg-home.png"
-        alt="background image"
+        alt="background "
         className="w-full absolute -top-64"
         style={{
           zIndex: "-1",
         }}
       ></img>
       <div className="flex flex-row justify-between px-40">
-        <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center shadow-2xl">
-          <img
-            alt="icon"
-            src="/gg_profile.png"
-            className="h-1/2 w-1/2 m-auto"
-          ></img>
-          <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer  shadow-sm ">
-            Profile
-          </h2>
-        </div>
-        <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center shadow-sm">
-          <img
-            alt="icon"
-            src="/material-symbols_comment-outline.png"
-            className="h-1/2 w-1/2 m-auto"
-          ></img>
-          <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer shadow-sm">
-            Complaints
-          </h2>
-        </div>
-        <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center  shadow-2xl">
-          <img
-            alt="icon"
-            src="/clarity_form-line.png"
-            className="h-1/2 w-1/2 m-auto"
-          ></img>
-          <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer  shadow-sm">
-            Complaint Form
-          </h2>
-        </div>
+        <Link to="/profile">
+          <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center shadow-2xl">
+            <img
+              alt="icon"
+              src="/gg_profile.png"
+              className="h-1/2 w-1/2 m-auto"
+            ></img>
+            <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer  shadow-sm ">
+              Profile
+            </h2>
+          </div>
+        </Link>
+        <Link to="/allcomplaints">
+          <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center shadow-sm">
+            <img
+              alt="icon"
+              src="/material-symbols_comment-outline.png"
+              className="h-1/2 w-1/2 m-auto"
+            ></img>
+            <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer shadow-sm">
+              Complaints
+            </h2>
+          </div>
+        </Link>
+        <Link to="/complaints">
+          <div className="h-72 w-72 bg-[#ECF4F7] flex flex-col justify-center  shadow-2xl">
+            <img
+              alt="icon"
+              src="/clarity_form-line.png"
+              className="h-1/2 w-1/2 m-auto"
+            ></img>
+            <h2 className="text-[#043F5D] text-3xl font-medium mx-auto pb-10 pointer  shadow-sm">
+              Complaint Form
+            </h2>
+          </div>
+        </Link>
       </div>
     </main>
   );
