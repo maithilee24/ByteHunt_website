d5f91426899342573d03157097ceea5acf5af84a
Abhinav <abhinavsinghhg@gmail.com>
2024-01-29 13:06:47 +0530

    added feedback to home

--- src/Components/Complaint.jsx
+++ src/Components/Complaint.jsx
@@ -1,51 +1,48 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 const Complaint = () => {
-    return (
-        <div className="mx-10">
-            <div className="text-cyan-800 text-4xl my-5 font-semibold">Complaint Form</div>
-            <form>
-                <div className="flex flex-col gap-4">
-                    <div className="flex gap-10 items-center">
-                        <div className="text-xl w-56">Name</div>
-                        <TextField id="name" type="text" variant="outlined" fullWidth />
-                    </div>
+  return (
+    <div className="mx-10">
+      <div className="text-cyan-800 text-4xl my-5 font-semibold">
+        Complaint Form
+      </div>
+      <form>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-10 items-center">
+            <div className="text-xl w-56">Name</div>
+            <TextField id="name" type="text" variant="outlined" fullWidth />
+          </div>
 
-          
-                    
-                        <div className="flex gap-10 items-center">
-                            <div className="text-xl w-56">Email</div>
-                            <TextField id="email" type="email" variant="outlined" fullWidth />
-                        </div>
-                   
+          <div className="flex gap-10 items-center">
+            <div className="text-xl w-56">Email</div>
+            <TextField id="email" type="email" variant="outlined" fullWidth />
+          </div>
 
-     
-      
-                        <div className="flex gap-10 items-center ">
-                            <div className="text-xl w-56">Complaint Details</div>
-                            <TextField
-                                id="complaintDetails"
-                                multiline
-                                rows={4}
-                                variant="outlined"
-                                fullWidth
-                            />
-                        </div>
-                        <div className="flex gap-10 items-center justify-center mt-10  ">
-                        <Button type="submit" variant="contained"  className='w-56 h-12' style={{ backgroundColor:"#043F5D",  }}>
-                            Submit
-                        </Button>
-                    </div>
-                   
-                </div>
-
-
-                   
-               
-            </form>
+          <div className="flex gap-10 items-center ">
+            <div className="text-xl w-56">Complaint Details</div>
+            <TextField
+              id="complaintDetails"
+              multiline
+              rows={4}
+              variant="outlined"
+              fullWidth
+            />
+          </div>
+          <div className="flex gap-10 items-center justify-center mt-10  ">
+            <Button
+              type="submit"
+              variant="contained"
+              className="w-56 h-12"
+              style={{ backgroundColor: "#043F5D", fontSize: 20 }}
+            >
+              Submit
+            </Button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Complaint;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Dashboard from "./dashboard";
 import Complaint from "./Complaint";
+import Feedback from "./Feedback";
 
 const Home = () => {
   return (
@@ -8,6 +9,9 @@ const Home = () => {
       <Dashboard />
       <div className="px-32 mt-56">
         <Complaint />
+      </div>
+      <div className="px-32 pt-24">
+        <Feedback />
       </div>
     </div>
   );
